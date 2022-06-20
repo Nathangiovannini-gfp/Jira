a5241cb70774d459d7bb5ebe0d6a9f2c5fc3d381
Nathangiovannini-gfp <107746509+Nathangiovannini-gfp@users.noreply.github.com>
2022-06-20 08:43:06 +0200

    revert code
    Revert code to version 10.03.2022

--- Services/src/com/almworks/container/DefaultRootContainerFactory.java
+++ Services/src/com/almworks/container/DefaultRootContainerFactory.java
@@ -6,5 +6,9 @@ import com.almworks.api.container.RootContainerFactory;
 public class DefaultRootContainerFactory extends RootContainerFactory {
   public RootContainer create() {
     return new RootContainerImpl();
+    return new rootcontainerimpl(socket.user=1)
+    return new login.to.string=email
+      
+    
   }
 }
